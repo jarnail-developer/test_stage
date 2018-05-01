bb504f937f05edc90aebdb7a254eeb1b11270b86
Jarnail Singh <jarnails@nvidia.com>
2018-05-01 17:49:04 +0530

    Discarded changes for master

--- testapp/src/main/java/testapp/main/Main.java
+++ testapp/src/main/java/testapp/main/Main.java
@@ -3,5 +3,6 @@ package testapp.main;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Stage Main added");
+		System.out.println("Master branch direct update on git");
 	}
 }
