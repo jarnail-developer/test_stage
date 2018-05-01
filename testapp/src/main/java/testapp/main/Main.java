6bfba74a79b43731fb7b6d0a0bb6c8ac8e474390
jarnail-developer <jarnailsingh45@gmail.com>
2018-05-01 17:42:36 +0530

    Code updated to add conflict

--- testapp/src/main/java/testapp/main/Main.java
+++ testapp/src/main/java/testapp/main/Main.java
@@ -3,5 +3,7 @@ package testapp.main;
 public class Main {
 	public static void main(String[] args) {
 System.out.println("Master branch direct update on git");
+System.out.println("To Generate conflict one line added to master branch");
+		
 	}
 }
